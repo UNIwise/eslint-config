4724e662db7730c7155e585090ea3300f964cacf
Jonas Tranberg <naueramant@users.noreply.github.com>
2021-09-14 14:30:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ module.exports = {
         "prettier",
     ],
     extends: [
-        "eslint:recommended"
+        "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:react/recommended",
         "plugin:react-hooks/recommended",
