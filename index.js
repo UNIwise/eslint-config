67a932591dccd1d8d6f87e752042f49da1fb4f7b
Benjamin <benjamin.zachariae@gmail.com>
2023-03-08 11:56:19 +0100

    typescript import resolver settings

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: true,
-        },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
     },
-    settings: {
-        react: {
-            version: "detect",
-        },
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    plugins: ["@typescript-eslint", "react", "import", "prettier"],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-        "plugin:prettier/recommended",
-    ],
-    rules: {
-        "react/prop-types": "off",
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
+    },
+  },
+  plugins: ["@typescript-eslint", "react", "import", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "react/prop-types": "off",
+  },
 };
